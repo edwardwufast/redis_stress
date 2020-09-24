5c20ca81b20bc39a34e2c2e618c8486e9e3bae20
Edward <edwardwufast@gmail.com>
2020-09-24 14:53:13 +0000

    add cluster test

--- benchmark.py
+++ benchmark.py
@@ -162,6 +162,84 @@ class send_set_randomkey_one_MB(commandset):
             key = "key" + value[:10]
             self.client.set(key, value)
 
+class send_set_randomkey_record_failure(commandset):
+    
+    def start(self):
+        timeout_start = time.time()
+        while time.time() < timeout_start + self.test_time:
+            random_number = random.random()
+            print(datetime.utcnow())
+            try:
+                response = self.client.set('key:' + str(random_number), random_number)
+                print(response)
+            except Exception as e:
+                print(e)
+            time.sleep(1)
+
+class cluster_slot_test(commandset):
+    
+    import dns.resolver
+
+    master_DNS = 'cluster-mode-enable-0002-003.ryi7vr.0001.apne1.cache.amazonaws.com'
+    replica_DNS = 'cluster-mode-enable-0002-001.ryi7vr.0001.apne1.cache.amazonaws.com'
+    key = 'key:0.5071153217346777'
+    log = 'log'
+
+    def init_client(self, host):
+        startup_nodes = [{"host": host, "port": "6379"}]
+        return RedisCluster(startup_nodes=startup_nodes, decode_responses=True, skip_full_coverage_check=True, readonly_mode=True)
+
+    def start(self):
+        timeout_start = time.time()
+        master = self.init_client(self.master_DNS)
+        replica = self.init_client(self.replica_DNS)
+        
+        f = open(self.log, "w+")
+
+        
+        while time.time() < timeout_start + self.test_time:
+            try:
+                current_time = datetime.utcnow()
+                f.write(str(current_time) + '\n')
+                print(current_time)
+                ping_response = replica.ping()
+                print(ping_response)
+                f.write(str(ping_response) + '\n')
+                get_response = replica.get(self.key)
+                print(f"replica: {get_response}")
+                f.write(f"replica: {get_response}\n")
+                try:
+                    master_get_response = master.get(self.key)
+                    print(f"master: {master_get_response}")
+                    f.write(f"master: {master_get_response}\n")
+                except Exception as e:
+                    print(e)
+                    f.write(str(e) + '\n')
+                    pass
+                cluster_slots = replica.cluster_slots()
+                print(cluster_slots)
+                f.write(str(cluster_slots) + '\n')
+                cluster_nodes = replica.cluster_nodes()
+                [ k.pop('slots') for k in cluster_nodes ]
+                for node in cluster_nodes:
+                    print(node)
+                    f.write(str(node) + '\n')
+                DNS_master_to_recover = [ ip.to_text() for ip in self.dns.resolver.query(self.master_DNS, 'A')][0]
+                print(f"DNS_master_to_recover: {DNS_master_to_recover}")
+                f.write(f"DNS_master_to_recover: {DNS_master_to_recover}\n")
+                DNS_replica_tobe_master = [ ip.to_text() for ip in self.dns.resolver.query(self.replica_DNS, 'A')][0]
+                print(f"DNS_replica_tobe_master: {DNS_replica_tobe_master}")
+                f.write(f"DNS_replica_tobe_master: {DNS_replica_tobe_master}\n\n")
+                print('\n')
+            except Exception as e:
+                print(e)
+                pass
+            time.sleep(1)
+        
+            
+            
+
+
 def create_dir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
